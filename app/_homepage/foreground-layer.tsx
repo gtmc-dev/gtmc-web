745ae9e4ee736b7e9751650607c7cb289044999d
4rcadia <97033226+Arcadi4@users.noreply.github.com>
2026-03-26 15:12:42 -0400

    feat(Homepage): Improve responsive experience

--- app/_homepage/foreground-layer.tsx
+++ app/_homepage/foreground-layer.tsx
@@ -40,10 +40,10 @@ export function ForegroundLayer({
       {/* 操作入口 */}
       <div
         className="
-          relative z-20 flex w-full animate-slide-up-fade flex-col items-stretch
-          justify-center gap-5 opacity-0 [animation-delay:1.4s]
+          relative z-20 flex w-full max-w-48 animate-slide-up-fade flex-col
+          items-stretch justify-center gap-5 opacity-0 [animation-delay:1.4s]
           fill-mode-forwards
-          sm:w-auto sm:flex-row sm:items-center
+          sm:w-full sm:max-w-full sm:flex-row sm:items-center
         ">
         <Link
           href="/articles"
@@ -64,13 +64,13 @@ export function ForegroundLayer({
             variant="primary"
             disabled={isAccessingDatabase}
             className="
-              flex h-12 w-full items-center justify-center px-8 py-3 text-sm
+              flex h-12 w-full items-center justify-center text-xs
               tracking-widest uppercase shadow-md transition-transform
               duration-300
-              hover:scale-105
+              hover:scale-102
               active:scale-95
               disabled:cursor-wait disabled:opacity-90
-              sm:w-auto
+              sm:w-auto sm:text-sm
             ">
             {isAccessingDatabase ? (
               <>
@@ -91,14 +91,13 @@ export function ForegroundLayer({
           <BrutalButton
             variant="ghost"
             className="
-              flex h-12 w-full items-center justify-center border
-              border-tech-main/40 bg-white/70 px-8 py-3 text-sm font-medium
+              flex h-12 w-full items-center justify-center text-xs font-medium
               tracking-widest text-tech-main-dark uppercase shadow-sm
               backdrop-blur-md transition-transform duration-300
-              hover:scale-105 hover:border-tech-main hover:bg-white
-              sm:w-auto
+              hover:scale-102 hover:border-tech-main hover:bg-tech-main/5
+              sm:w-auto sm:text-sm
             ">
-            /{"/"} LOGIN
+            {"//"} LOGIN (GITHUB)
           </BrutalButton>
         </Link>
       </div>
@@ -117,9 +116,9 @@ export function ForegroundLayer({
               flex size-8 items-center justify-center
               ${i === 3
                 ? `
-                    border-2 border-tech-main-dark bg-tech-main/10
-                    shadow-[0_0_8px_rgba(96,112,143,0.3)]
-                  `
+                  border-2 border-tech-main-dark bg-tech-main/10
+                  shadow-[0_0_8px_rgba(96,112,143,0.3)]
+                `
                 : `border border-tech-main/40`
               }
             `}>
