8cb386fc7a423c430ace76eaa443fd246196a64e
4rcadia <97033226+Arcadi4@users.noreply.github.com>
2026-03-26 00:17:06 -0400

    chore: Remove unused diff test scripts

--- test-diff-words.ts
+++
@@ -1,5 +0,0 @@
-import { diffWords } from "diff"
-const current = "Hello world\r\nThis is a test"
-const incoming = "Hello world\nThis is a test with difference"
-const diffs = diffWords(incoming, current)
-console.log(diffs)
